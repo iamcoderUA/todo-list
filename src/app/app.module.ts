918d51cbfaa8a31beb6037280cfd93e6fc8fb424
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-16 01:09:45 +0200

    feat(core): milestone 9, create core module & services folder, transfer todo-items service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { TodosModule } from './todos/todos.module';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     TodosModule,
     AppRoutingModule
   ],
