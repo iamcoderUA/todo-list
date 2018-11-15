c8bfd9c858c4bffb02df31809d6c554667c83a8e
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-16 01:46:39 +0200

    feat(core:models): create todo-items model

---
+++ src/app/core/models/todo-items.ts
@@ -0,0 +1,13 @@
+export class TodoItemsModel {
+  id: number = Math.floor(Math.random() * (10000 - 1 + 1)) + 1;
+  title: '';
+  complete = false;
+
+  constructor(obj?: any) {
+    for (const field in obj) {
+      if (typeof this[field] !== 'undefined') {
+        this[field] = obj && obj[field];
+      }
+    }
+  }
+}
