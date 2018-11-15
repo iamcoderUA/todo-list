ae2d81136df5f96d48d573a54ae2f84362ce9491
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-16 00:34:52 +0200

    feat(todos): create active-items module

---
+++ src/app/todos/active-items/active-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-items',
+  templateUrl: './active-items.component.html',
+  styleUrls: ['./active-items.component.scss']
+})
+export class ActiveItemsComponent {
+}
+
+
