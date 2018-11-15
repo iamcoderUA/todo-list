53a73bfa9e57e1b45de16e5c3d9b91c275bc8e8d
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-16 00:36:08 +0200

    refactor(todos): milestone 6, add children routes

--- src/app/todos/todos-routing.module.ts
+++ src/app/todos/todos-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ActiveItemsComponent } from './active-items/active-items.component';
+import { AllItemsComponent } from './all-items/all-items.component';
+import { CompletedItemsComponent } from './completed-items/completed-items.component';
+
 import { TodosComponent } from './todos.component';
 
+
 const routes: Routes = [
-  { path: '', component: TodosComponent}
+  {
+    path: '',
+    component: TodosComponent,
+    children:
+    [
+       { path: '', component: AllItemsComponent},
+       { path: 'active', component: ActiveItemsComponent},
+       { path: 'completed', component: CompletedItemsComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 
+import { ActiveItemsModule } from './active-items/active-items.module';
+import { AllItemsModule } from './all-items/all-items.module';
+import { CompletedItemsModule } from './completed-items/completed-items.module';
 import { FooterModule } from './footer/footer.module';
 import { HeaderModule } from './header/header.module';
 
@@ -7,13 +10,17 @@ import { TodosRoutingModule } from './todos-routing.module';
 
 import { TodosComponent } from './todos.component';
 
+
 @NgModule({
   declarations: [
-    TodosComponent
+    TodosComponent,
   ],
   imports: [
     FooterModule,
     HeaderModule,
+    AllItemsModule,
+    ActiveItemsModule,
+    CompletedItemsModule,
     TodosRoutingModule
   ],
 })
