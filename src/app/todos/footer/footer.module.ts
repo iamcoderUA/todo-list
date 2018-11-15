09dff699576a0d5ab29c5d9b20600084e76fd57e
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-16 00:58:07 +0200

    feat(todos:footer): milestone 7, add navigation menu

--- src/app/todos/footer/footer.module.ts
+++ src/app/todos/footer/footer.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+
 import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [
     FooterComponent
+  ],
+  imports: [
+    RouterModule
   ],
   exports: [
     FooterComponent
