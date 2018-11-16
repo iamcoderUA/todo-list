a633b6fa7127a5cc3a1dbb382c9a1243a9d8b64f
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-17 00:09:29 +0200

    feat(core:mock): milestone 11, create mock-todo-items

---
+++ src/app/core/mock/mock-todo-items.ts
@@ -0,0 +1,9 @@
+import { TodoItemsModel } from '../models/todo-items';
+
+export const TODO_ITEMS: TodoItemsModel[] = [
+  new TodoItemsModel({title: 'Call John'}),
+  new TodoItemsModel({title: 'Call Mary'}),
+  new TodoItemsModel({title: 'Call Scott'}),
+  new TodoItemsModel({title: 'Call Ben'}),
+  new TodoItemsModel({title: 'Call Kira'}),
+];
