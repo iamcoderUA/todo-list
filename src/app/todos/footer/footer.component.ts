4e1f458025906d0e19538b2c524def2d1d272714
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-17 17:48:17 +0200

    feat(todos:footer): milestone 18, add todo-items counter

--- src/app/todos/footer/footer.component.ts
+++ src/app/todos/footer/footer.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+import { TodoItemsModel } from '../../core/models/todo-items';
+
+import { TodoItemsService } from '../../core/services/todo-items.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  constructor(
+    private todoItemsService: TodoItemsService,
+  ) {
+  }
+
+  get todoItems () {
+    return this.todoItemsService.todoItems as TodoItemsModel[];
+  }
 }
 
 
