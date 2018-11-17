23d0876c36b0fc660b1fd1b040a4cd2285e44f78
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-17 15:07:58 +0200

    refactor(core:mock): update initial data for todo-items mock

--- src/app/core/mock/mock-todo-items.ts
+++ src/app/core/mock/mock-todo-items.ts
@@ -1,8 +1,8 @@
 import { TodoItemsModel } from '../models/todo-items';
 
 export const TODO_ITEMS: TodoItemsModel[] = [
-  new TodoItemsModel({title: 'Call John'}),
-  new TodoItemsModel({title: 'Call Mary'}),
+  new TodoItemsModel({title: 'Call John', complete: true}),
+  new TodoItemsModel({title: 'Call Mary', complete: true}),
   new TodoItemsModel({title: 'Call Scott'}),
   new TodoItemsModel({title: 'Call Ben'}),
   new TodoItemsModel({title: 'Call Kira'}),
