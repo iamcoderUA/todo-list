b4c57579adeeb931f705661fa5bedf239b5ac5b1
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-23 17:05:56 +0200

    feat(ngxs:todo-items): milestone 7, create Actions for fetch todo-items

--- src/app/ngxs/todo-items/todo-items.state.ts
+++ src/app/ngxs/todo-items/todo-items.state.ts
@@ -1,6 +1,12 @@
-import {Selector, State} from '@ngxs/store';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+
+import { catchError, tap } from 'rxjs/operators';
 
 import { TodoItemsModel } from '../../core/models/todo-items';
+import { TodoItemsRequestsService } from '../../core/services/todo-items-requests.service';
+
+import * as todoItemsActions from './todo-items.actions';
+
 
 export interface TodoItemsStateModel {
   todoItems:
@@ -27,5 +33,42 @@ export class TodoItemsState {
   @Selector()
   static getTodoItemsIdsCount(state: TodoItemsStateModel) {
     return state.todoItemsIds.length;
+  }
+
+  constructor(
+    private todoItemsRequestsService: TodoItemsRequestsService
+  ) {
+  }
+
+  @Action(todoItemsActions.FetchTodoItems)
+  fetchTodoItems(
+    {dispatch}: StateContext<TodoItemsStateModel>
+  ) {
+    return this.todoItemsRequestsService.fetchTodoItems().pipe(
+      tap(todoItems => dispatch(new todoItemsActions.FetchTodoItemSuccess(todoItems))),
+      catchError(error => dispatch(new todoItemsActions.FetchTodoItemFail(error)))
+    );
+  }
+
+  @Action(todoItemsActions.FetchTodoItemSuccess)
+  fetchTodoItemSuccess(
+    {setState}: StateContext<TodoItemsStateModel>,
+    {payload: todoItems}: todoItemsActions.FetchTodoItemSuccess
+  ) {
+    setState({
+      todoItems: todoItems.reduce((acc, todoItem) => ({
+        ...acc,
+        [todoItem.id]: todoItem
+      }), {}),
+      todoItemsIds: todoItems.map(item => item.id)
+    });
+  }
+
+  @Action(todoItemsActions.FetchTodoItemFail)
+  fetchTodoItemFail(
+    ctx: StateContext<TodoItemsStateModel>,
+    {payload: error}: todoItemsActions.FetchTodoItemFail
+  ) {
+    console.error(error);
   }
 }
