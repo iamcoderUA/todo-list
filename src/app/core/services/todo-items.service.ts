ddd625b91aab7f62634670699719b2356252bbe7
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-24 23:03:49 +0200

    feat(core:services): milestone 11, dispatch remaining Actions

--- src/app/core/services/todo-items.service.ts
+++ src/app/core/services/todo-items.service.ts
@@ -3,7 +3,12 @@ import { Injectable } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs/internal/Observable';
 
-import { FetchTodoItems } from '../../ngxs/todo-items/todo-items.actions';
+import {
+  AddTodoItem,
+  DeleteTodoItem,
+  FetchTodoItems,
+  ToggleTodoItemComplete
+} from '../../ngxs/todo-items/todo-items.actions';
 import { TodoItemsState } from '../../ngxs/todo-items/todo-items.state';
 
 import { TodoItemsModel } from '../models/todo-items';
@@ -26,11 +31,14 @@ export class TodoItemsService {
   }
 
   addTodoItem(newTodoItem: TodoItemsModel) {
+    this.store.dispatch(new AddTodoItem(newTodoItem));
   }
 
   toggleTodoItemComplete(id: number) {
+    this.store.dispatch(new ToggleTodoItemComplete(id));
   }
 
   deleteTodoItem(id: number) {
+    this.store.dispatch(new DeleteTodoItem(id));
   }
 }
