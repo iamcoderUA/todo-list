58bbae6175ce096d5cedc326f6d3af363d5d5595
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-12-06 00:15:57 +0200

    feat(core:services): create auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { UserModel } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  authToken$: Observable<string>;
+  isGuest$: Observable<boolean>;
+
+  constructor(
+    private httpClient: HttpClient,
+  ) {
+  }
+
+  login(userData) {
+    return this.httpClient.post<UserModel>('auth/login', userData);
+  }
+
+  getAuthUser() {
+    return this.httpClient.get<UserModel>('auth/user');
+  }
+}
